5b2bd1782e5d048df9a3c8b6997b5fc4e6b964f7
Daniel Bennett <62999631+dannnyben@users.noreply.github.com>
2023-11-21 15:31:15 +0000

    Create HomePage.php

---
+++ HomePage.php
@@ -0,0 +1,108 @@
+<html>
+    <head>
+        <title> Home </title>
+        <link rel="stylesheet" href="HomePage.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <div id="LogoContainer">
+            <img id="Logo" src="BitzLogo.png" alt="Bitz">
+        </div>
+
+        <div class="dropdown">
+            <button class="dropbtn"> Account </button>
+                <div class="dropdown-content">
+                    <li>
+                        <a href="LogInPage.php">
+                            <i class="material-symbols-outlined"> logout </i> Logout
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#UserAccount">
+                            <i class="material-symbols-outlined"> badge </i> User Account
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#AccountSettings">
+                            <i class="material-symbols-outlined"> settings </i> Account Settings
+                        </a>
+                    </li>
+                </div> 
+            </div>
+        </head>
+    <body>
+
+    <ul>
+        <li>
+            <a class="active">
+                <i class="material-symbols-outlined"> home </i> Home
+            </a>
+        </li>
+        <li>
+            <a href="#CPU">
+                <i class="material-symbols-outlined"> memory </i> CPU
+            </a>
+        </li>
+        <li>
+            <a href="#RAM">
+                <i class="material-symbols-outlined"> memory </i> RAM
+            </a>
+        </li>
+        <li>
+            <a href="#GPU">
+                <i class="material-symbols-outlined"> memory </i> GPU
+            </a>
+        </li>
+        <li>
+            <a href="#PSU">
+                <i class="material-symbols-outlined"> memory </i> PSU
+            </a>
+        </li>
+        <li>
+            <a href="#Coolers">
+                <i class="material-symbols-outlined"> memory </i> Coolers
+            </a>
+        </li>
+        <li>
+            <a href="#SHPC">
+                <i class="material-symbols-outlined"> devices </i> Second-Hand PC's
+            </a>
+        </li>
+        <li>
+            <a href="#Monitors">
+                <i class="material-symbols-outlined"> desktop_windows </i> Monitors
+            </a>
+        </li>
+        <li>
+            <a href="#LaptopsW/L">
+                <i class="material-symbols-outlined"> computer </i> Laptops Windows/Linux
+            </a>
+        </li>
+        <li>
+            <a href="#AndroidPhones">
+                <i class="material-symbols-outlined"> smartphone </i> Android Phones
+            </a>
+        </li>
+        <li>
+            <a href="#Macbooks">
+                <i class="material-symbols-outlined"> laptop_mac </i> Macbooks
+            </a>
+        </li>
+        <li>
+            <a href="#Iphones">
+                <i class="material-symbols-outlined"> phone_iphone </i> Iphones
+            </a>
+        </li>
+        <li style="float:right;">
+            <a href="AddListingPage.php">
+                <i class="material-symbols-outlined"> add </i> Add Listing
+            </a>
+        </li>
+        
+    </ul>
+
+    <div id="RandomPc">
+
+    </div>
+    </body>
+</html>
