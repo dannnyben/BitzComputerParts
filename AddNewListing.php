c0950af54ffaa2c0a2b58184d26a9008266359de
Daniel Bennett <62999631+dannnyben@users.noreply.github.com>
2023-11-22 14:33:07 +0000

    Update AddNewListing.php

--- AddNewListing.php
+++ AddNewListing.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title> Add Listing </title>
-        <link rel="stylesheet" href="HomePage.css">
+        <link rel="stylesheet" href="MainSite.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
         <link href='https://fonts.googleapis.com/css?family=Varela Round' rel='stylesheet'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
