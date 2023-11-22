0c087cafb4472a01f7abc410cf60159caf6025d2
Daniel Bennett <62999631+dannnyben@users.noreply.github.com>
2023-11-22 14:32:26 +0000

    Update CheckUserLogIn.php

--- CheckUserLogIn.php
+++ CheckUserLogIn.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title> Log In </title>
-        <link rel="stylesheet" href="LogInPage.css">
+        <link rel="stylesheet" href="LogIn.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
